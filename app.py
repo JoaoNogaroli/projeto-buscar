a2971824164c5556a803025bc7e2af25b1bbf5b8
JoaoNogaroli <victhornogaroli@gmail.com>
2021-02-17 13:42:44 -0300

    a

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request, session
+import json
 import os
+import json
+from selenium import webdriver
+import time
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+import re
+
+
+
 app = Flask(__name__)
 
 port = int(os.environ.get("PORT", 5000))
 
 
+def teste():
+    a =1
+    return a
+
+
 @app.route('/')
 def index():
-    return "<h1>olá</h1>"
+    a = teste()
+    return render_template('index', a=a)
 
-
-@app.route('/sobre')
-def sobre():
-    return "<h1>sobre</h1>"
 
 if __name__== "__main__":
     app.run(debug=True, port=port)
