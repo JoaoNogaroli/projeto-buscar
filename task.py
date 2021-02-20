c5137362007ac855742a3e282f85cb85d8989fde
JoaoNogaroli <victhornogaroli@gmail.com>
2021-02-19 21:39:32 -0300

    teste

--- task.py
+++ task.py
@@ -13,7 +13,7 @@ import os
 import sqlalchemy
 import time
 import re
-import pyrebase
+import firebase
 
 celery = Celery('task',broker=CELERY_BROKER_BACKEND, backend=result_backend)
 
@@ -21,17 +21,18 @@ celery = Celery('task',broker=CELERY_BROKER_BACKEND, backend=result_backend)
 firebaseConfig = {
     "apiKey": "AIzaSyD-5dd-c9GUcNnJbzZwJsGyj5Mi077EJv0",
     "authDomain": "celery-db.firebaseapp.com",
+    "databaseURL": "https://celery-db-default-rtdb.firebaseio.com",
     "projectId": "celery-db",
     "storageBucket": "celery-db.appspot.com",
     "messagingSenderId": "263395259912",
     "appId": "1:263395259912:web:a45cc33a1140500971c3bd",
     "measurementId": "G-6JT7B5X9DY"
-  }
-firebase = pyrebase.initialize_app(firebaseConfig)
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 database = firebase.database()
-
-
 
 
 @celery.task
