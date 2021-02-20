070952bed0c337db7cf2eb2e384e75bca05a8fc8
JoaoNogaroli <victhornogaroli@gmail.com>
2021-02-20 12:29:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from celery import Celery
 from celeryconfig import CELERY_BROKER_BACKEND,result_backend
+from celery.result import AsyncResult
+
 from flask import Flask, render_template, url_for, request, session, redirect
 import json
 import os
@@ -16,18 +18,19 @@ port = int(os.environ.get("PORT", 5000))
 def pegar():
     word = request.form['jojo']
     
-    a = debug_task(word).AsyncResult(task_id)
-    b = a.get()
-    """time.sleep(1)
+    a = debug_task.delay(word)
+    a_id = debug_task(word).AsyncResult(task_id)
+    b = a_id.get()
+    "time.sleep(1)
     while a.state not in ('SUCCESS', 'FAILURE'): 
-        a_value = (a.get())[0]
-        b_value = (a.get())[1] 
-        return render_template('teste.html', progress = a.status, a = a_value, b= b_value)
+        a_value = "Esperando"
+        b_value = "Esperando"
+        return render_template('teste.html', progress =b, a = a_value, b= b_value)
     else:
         a_value = (a.get())[0]
-        b_value = (a.get())[1]   """   
+        b_value = (a.get())[1] 
 
-    return render_template('teste.html', progress=b)
+    return render_template('teste.html', progress=b, a = a_value, b= b_value)
 
 @app.route('/teste')
 def refresh():
